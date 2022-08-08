4cfe203f3eb7d85163add452f7dbf5807b51aded
metricRGS <73303719+metricRGS@users.noreply.github.com>
2022-08-08 12:27:58 +0200

    Update ModConfig.java

--- src/main/java/net/sxmaa/timelimiter/ModConfig.java
+++ src/main/java/net/sxmaa/timelimiter/ModConfig.java
@@ -1,49 +1,49 @@
 package net.sxmaa.timelimiter;
 
 import java.io.File;
-
-import cpw.mods.fml.client.event.ConfigChangedEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.config.*;
 
-public class ModConfig
+public class ModConfig extends Configuration
 {
-    private static Configuration configuration;
-
     // config elements
-    public static boolean doPlayerTimeLimit;
-    public static int playerTimeLimitResetInterval;
-    public static int playerTimeLimit;
+    public static boolean doPlayerTimeLimit = true;
+    public static int playerTimeLimitResetInterval = 86400;
+    public static int playerTimeLimit = 7200;
 
-    public static void init(String configDir) {
-        
-        if(configuration == null) {
-            File path = new File(configDir + Tags.MODID + ".cfg");
+    public ModConfig(String configDir){
+        super(
+            new File(configDir + Tags.MODID + ".cfg")
+        );
 
-            configuration = new Configuration(path);
-            loadConfiguration();
-        }
+        this.load();
+        loadConfiguration();
+        this.save();
     }
 
-    private static void loadConfiguration() {
-        
-        doPlayerTimeLimit = configuration.getBoolean("do player time limit", Configuration.CATEGORY_GENERAL, true, "Limit the time every player can spend on the server");
-        playerTimeLimitResetInterval = configuration.getInt("time limit reset interval", Configuration.CATEGORY_GENERAL, 86400, 0, 604800 /* 1 week*/, "Time to pass between each reset of a players time wallet to the value of \"time limit\"");
-        playerTimeLimit = configuration.getInt("player time limit", Configuration.CATEGORY_GENERAL, 7200 /*2 hours */, 0, playerTimeLimitResetInterval, "Time each player can spend on the server within one interval.");
+    private void loadConfiguration() {
 
-        if(configuration.hasChanged()) {
-            configuration.save();
-        }
+        doPlayerTimeLimit = this.getBoolean(
+            "do player time limit",
+            Configuration.CATEGORY_GENERAL,
+            true,
+            "Limit the time every player can spend on the server");
+
+        playerTimeLimitResetInterval = this.getInt(
+            "time limit reset interval",
+            Configuration.CATEGORY_GENERAL,
+            playerTimeLimitResetInterval,
+            0,
+            604800 /* 1 week*/,
+            "Time to pass between each reset of a players time wallet to the value of \"time limit\"");
+
+        playerTimeLimit = this.getInt(
+            "player time limit",
+            Configuration.CATEGORY_GENERAL,
+            playerTimeLimit /*2 hours */,
+            0,
+            playerTimeLimitResetInterval,
+            "Time each player can spend on the server within one interval.");
+
     }
-
-    @SubscribeEvent
-    public static void onConfigurationChangeEvent(ConfigChangedEvent.OnConfigChangedEvent event) {
-
-        if(event.modID.equalsIgnoreCase(Tags.MODID)) {
-            loadConfiguration();
-        }
-    }
-
-    public static Configuration getConfiguration() { return configuration; }
 }
 
